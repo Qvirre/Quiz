008f6cef6f19c1daf13fb72cd511fd048ae657d3
Qvirre <anders_quist@hotmail.com>
2024-12-04 23:14:34 +0100

    Improve validation.

--- anders.js
+++ anders.js
@@ -24,6 +24,7 @@ class Facit {
    // Add methods like normal functions:  
 }
 
+// Create a new object from the class Facit
 const quizFacit = new Facit(
    "Orange",
    "Markup",
@@ -33,17 +34,17 @@ const quizFacit = new Facit(
 );
 
 function isTextCorrect(questionId, correctValue) {
-   const answer = document.getElementById(questionId).value;
+   const answer = document.querySelector(questionId).value;
    return answer.toLowerCase() === correctValue.toLowerCase();
 }
 
 function isSelectCorrect(questionId, correctValue) {
-   const answer = document.getElementById(questionId).value;
+   const answer = document.querySelector(questionId).value;
    return answer.toLowerCase() === correctValue.toLowerCase();
 }
 
 function isSelected(questionId) {
-   const answer = document.getElementById(questionId).value;
+   const answer = document.querySelector(questionId).value;
    return answer.toLowerCase() !== "";
 }
 
@@ -75,14 +76,14 @@ function arraysEqual(arr1, arr2) {
 
 /* Check if Emty */
 function checkEmty(name) {
-   let elem = document.getElementById(name);
+   let elem = document.querySelector(name);
 
    return (elem.value === "");
 }
 
 /* Only letters are allowed */
 function checkPatternText(name) {
-   let elem = document.getElementById(name);
+   let elem = document.querySelector(name);
 
    // Allow A-Z, a-z min 1 char.
    let re = /^[a-zA-Z]+$/;
@@ -92,7 +93,7 @@ function checkPatternText(name) {
 
 /* Enter a valid email address */
 function checkPatternEmail(name) {
-   let elem = document.getElementById(name);
+   let elem = document.querySelector(name);
 
    // Allow
    let re = /[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$/;
@@ -102,9 +103,9 @@ function checkPatternEmail(name) {
 
 
 /* Event */
-const submitButton = document.getElementById("submit");
-const resetButton  = document.getElementById("reset");
-const facitButton  = document.getElementById("facit");
+const submitButton = document.querySelector("#submit");
+const resetButton  = document.querySelector("#reset");
+const facitButton  = document.querySelector("#facit");
 
 let fNameValid = false;
 let lNameValid = false;
@@ -114,38 +115,52 @@ let q2Valid    = false;
 let q3Valid    = false;
 let q4Valid    = false;
 
+function setFlagsToFalse () {
+   fNameValid = false;
+   lNameValid = false;
+   emailValid = false;
+   q1Valid    = false;
+   q2Valid    = false;
+   q3Valid    = false;
+   q4Valid    = false;
+   document.querySelector("#divError").style.color = "red";
+}
+
+/* submitButton addEventListener */
 submitButton.addEventListener("click", (event) => {
+
+   setFlagsToFalse();
 
    /* Visitor Information */
 
-   if (checkEmty("firstName")) {
-      document.getElementById("divError").innerText = "Fill in your first name!";
-   } else if (!checkPatternText("firstName")) {
-      document.getElementById("divError").innerText = "Only letters are allowed!";  
+   if (checkEmty("#firstName")) {
+      document.querySelector("#divError").innerText = "Fill in your first name!";
+   } else if (!checkPatternText("#firstName")) {
+      document.querySelector("#divError").innerText = "Only letters are allowed!";  
    } else {
       fNameValid = true; 
-      document.getElementById("divError").innerText = "";
+      document.querySelector("#divError").innerText = "";
    }
 
    if (fNameValid === true) {
-      if (checkEmty("lastName")) {
-         document.getElementById("divError").innerText = "Fill in your last name!";
-      } else if (!checkPatternText("lastName")) {
-         document.getElementById("divError").innerText = "Only letters are allowed!";  
+      if (checkEmty("#lastName")) {
+         document.querySelector("#divError").innerText = "Fill in your last name!";
+      } else if (!checkPatternText("#lastName")) {
+         document.querySelector("#divError").innerText = "Only letters are allowed!";  
       } else {
          lNameValid = true;
-         document.getElementById("divError").innerText = "";
+         document.querySelector("#divError").innerText = "";
       }
    }
 
    if (lNameValid === true) {
-      if (checkEmty("email")) {
-         document.getElementById("divError").innerText = "Fill in your email address!";
-      } else if (!checkPatternEmail("email")) {
-         document.getElementById("divError").innerText = "Enter a valid email address!";  
+      if (checkEmty("#email")) {
+         document.querySelector("#divError").innerText = "Fill in your email address!";
+      } else if (!checkPatternEmail("#email")) {
+         document.querySelector("#divError").innerText = "Enter a valid email address!";  
       } else {
          emailValid = true;
-         document.getElementById("divError").innerText = "";
+         document.querySelector("#divError").innerText = "";
       }
    }
 
@@ -153,18 +168,18 @@ submitButton.addEventListener("click", (event) => {
    let counter = 0;
 
    /* Question 1 */
-   if (isTextCorrect("q1", "Orange")) {
+   if (isTextCorrect("#q1", "Orange")) {
       counter++;
    };
 
    if (emailValid === true) {
-      if (checkEmty("q1")) {
-         document.getElementById("divError").innerText = "Question 1 is required!";
-      } else if (!checkPatternText("q1")) {
-         document.getElementById("divError").innerText = "Only letters are allowed!";  
+      if (checkEmty("#q1")) {
+         document.querySelector("#divError").innerText = "Question 1 is required!";
+      } else if (!checkPatternText("#q1")) {
+         document.querySelector("#divError").innerText = "Only letters are allowed!";  
       } else {
          q1Valid = true;
-         document.getElementById("divError").innerText = "";
+         document.querySelector("#divError").innerText = "";
       }
    }
 
@@ -175,10 +190,10 @@ submitButton.addEventListener("click", (event) => {
 
    if (q1Valid === true) {
       if (!isCheckt("q2")) {
-         document.getElementById("divError").innerText = "Question 2 is required!";
+         document.querySelector("#divError").innerText = "Question 2 is required!";
       } else {
          q2Valid = true;
-         document.getElementById("divError").innerText = "";
+         document.querySelector("#divError").innerText = "";
       }
    }
 
@@ -190,90 +205,95 @@ submitButton.addEventListener("click", (event) => {
 
    if (q2Valid === true) {
       if (!isCheckt("q3")) {
-         document.getElementById("divError").innerText = "Question 3 is required!";
+         document.querySelector("#divError").innerText = "Question 3 is required!";
       } else {
          q3Valid = true;
-         document.getElementById("divError").innerText = "";
+         document.querySelector("#divError").innerText = "";
       }
    }
    
    /* Question 4 */
-   if (isSelectCorrect("q4", "Brendan Eich")) {
+   if (isSelectCorrect("#q4", "Brendan Eich")) {
       counter++;
    };
 
    if (q3Valid === true) {
-      if (!isSelected("q4")) {
-         document.getElementById("divError").innerText = "Question 4 is required!";
+      if (!isSelected("#q4")) {
+         document.querySelector("#divError").innerText = "Question 4 is required!";
       } else {
          q4Valid = true;
-         document.getElementById("divError").innerText = "";
+         document.querySelector("#divError").innerText = "";
       }
    }
 
    /* Question 5 */
-   if (isTextCorrect("q5", "Paris")) {
+   if (isTextCorrect("#q5", "Paris")) {
       counter++;
    };
 
    if (q4Valid === true) {
-      if (checkEmty("q5")) {
-         document.getElementById("divError").innerText = "Question 5 is required!";
-      } else if (!checkPatternText("q5")) {
-         document.getElementById("divError").innerText = "Only letters are allowed!";  
+      if (checkEmty("#q5")) {
+         document.querySelector("#divError").innerText = "Question 5 is required!";
+      } else if (!checkPatternText("#q5")) {
+         document.querySelector("#divError").innerText = "Only letters are allowed!";  
       } else {
-         document.getElementById("divError").innerText = "Quiz submitted successfully!";
+         document.querySelector("#divError").style.color = "blue";
+         document.querySelector("#divError").innerText = "Quiz submitted successfully!";
       }
    }
    
    /* You scorde. */
-   document.getElementById("divScore").innerText = "You scorde " + counter + " out of 5.";
+   document.querySelector("#divScore").innerText = "You scorde " + counter + " out of 5.";
 
    /* Opinion on results */
    switch(counter) {
       case 5:
-         document.getElementById("divScore").innerText += "\n Congratulations, you answered all the questions correctly.";
+         document.querySelector("#divScore").innerText += "\n Congratulations, you answered all the questions correctly.";
          break;
       case 4:
-         document.getElementById("divScore").innerText += "\n It was close, you answered wrong on one question.";
+         document.querySelector("#divScore").innerText += "\n It was close, you answered wrong on one question.";
          break;
       case 3:
       case 2:
-         document.getElementById("divScore").innerText += "\n Better luck next time.";
+         document.querySelector("#divScore").innerText += "\n Better luck next time.";
          break;  
       default:
-         document.getElementById("divScore").innerText += "\n Quizzes are not for you.";
+         document.querySelector("#divScore").innerText += "\n Quizzes are not for you.";
    } 
   
 });
 
+/* resetButton addEventListener */
 resetButton.addEventListener("click", (event) => {
-   fNameValid = false;
-   lNameValid = false;
-   emailValid = false;
-   q1Valid    = false;
-   q2Valid    = false;
-   q3Valid    = false;
-   q4Valid    = false;   
-   document.getElementById("divError").innerText = ""; 
-   document.getElementById("divScore").innerText = "";
-   document.getElementById("divFacit").innerText = ""; 
+   setFlagsToFalse();
+   document.querySelector("#divError").innerText = ""; 
+   document.querySelector("#divScore").innerText = "";
+   document.querySelector("#divFacit").innerText = "";
 });
 
+/* facitButton addEventListener */
 facitButton.addEventListener("click", (event) => {
 
-   /* Mix text and variables with template literals */
-   const content = `
-   <h2>Correct Answers</h2>
-   <ol>
-      <li>${quizFacit.answerOne}</li>
-      <li>${quizFacit.answerTwo}</li>
-      <li>${quizFacit.answerThree}</li>
-      <li>${quizFacit.answerFoure}</li>
-      <li>${quizFacit.answerFive}</li>
-   </ol>  
-   `;
+   if (document.querySelector("#divFacit").innerHTML == "") {
 
-   document.getElementById("divFacit").innerHTML = content; 
+      /* Mix text and variables with template literals */
+      const content = `
+      <h2>Correct Answers</h2>
+      <ol>
+         <li>${quizFacit.answerOne}</li>
+         <li>${quizFacit.answerTwo}</li>
+         <li>${quizFacit.answerThree}</li>
+         <li>${quizFacit.answerFoure}</li>
+         <li>${quizFacit.answerFive}</li>
+      </ol>  
+      `;
+      // document.getElementById("divFacit").innerHTML = content;
+      document.querySelector("#divFacit").innerHTML = content;
+      document.querySelector("#facit").setAttribute("value", "Hide Answers");  
+   } else {
+      document.querySelector("#divFacit").innerHTML = "";
+      document.querySelector("#facit").setAttribute("value", "Show Correct Answers");
+   }
+   
 });
 
